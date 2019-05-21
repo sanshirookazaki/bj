9380e1d4a5016b16c3fd62c0b3f0133161da9a00
sanshirookazaki <sanshirookazaki@gmail.com>
2019-05-21 23:50:29 +0900

    update builder

--- card/init.go
+++ card/init.go
@@ -1,14 +1,28 @@
 package card
 
+type Talon interface{}
+
 type talonBuiler struct {
 	card map[string]int // "カードの種類":残り
 	num  int
 }
 
-func New() TalonBuiler {
+func New() TalonBuilder {
 	return &talonBuiler{}
 }
 
-type TalonBuilder interface{
-	Build Talon
+type TalonBuilder interface {
+	Build() Talon
+}
+
+func (tb *talonBuiler) Build() Talon {
+	return &talon{
+		card: tb.card,
+		num:  tb.num,
+	}
+}
+
+type talon struct {
+	card map[string]int
+	num  int
 }
