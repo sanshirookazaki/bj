94bdf201f82d180ff4dd4162e3e8093eb40d9abc
sanshirookazaki <sanshirookazaki@gmail.com>
2019-06-05 01:13:07 +0900

    add card test

---
+++ card/card_test.go
@@ -0,0 +1,22 @@
+package card
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Testcontains(t *testing.T) {
+	a := []string{"a", "bb", "ccc"}
+	if !contains(a, "bb") {
+		t.Fatalf("error: %v", a)
+	}
+}
+
+func Testremove(t *testing.T) {
+	a := []string{"a", "bb", "ccc"}
+	result := remove(a, "ccc")
+	expect := []string{"a", "bb"}
+	if !reflect.DeepEqual(result, expect) {
+		t.Fatalf("error: expect %v but result %v", expect, result)
+	}
+}
