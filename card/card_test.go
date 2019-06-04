b0bf3dbe9dec0662ac81ab22f0057a9b72df4139
sanshirookazaki <sanshirookazaki@gmail.com>
2019-06-05 01:22:10 +0900

    update card test

--- card/card_test.go
+++ card/card_test.go
@@ -5,18 +5,20 @@ import (
 	"testing"
 )
 
-func Testcontains(t *testing.T) {
+func TestContains(t *testing.T) {
 	a := []string{"a", "bb", "ccc"}
 	if !contains(a, "bb") {
 		t.Fatalf("error: %v", a)
 	}
+	t.Log("Pass contains func")
 }
 
-func Testremove(t *testing.T) {
+func TestRemove(t *testing.T) {
 	a := []string{"a", "bb", "ccc"}
 	result := remove(a, "ccc")
 	expect := []string{"a", "bb"}
 	if !reflect.DeepEqual(result, expect) {
 		t.Fatalf("error: expect %v but result %v", expect, result)
 	}
+	t.Log("Pass remove func")
 }
