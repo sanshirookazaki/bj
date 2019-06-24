8b5ef56e9247baba6ce717679c3484828a025e0d
sanshirookazaki <sanshirookazaki@gmail.com>
2019-06-24 23:17:18 +0900

    update test

--- player/player_test.go
+++ player/player_test.go
@@ -21,16 +21,17 @@ func TestSum(t *testing.T) {
 }
 
 func TestCompute(t *testing.T) {
-	playerbuilder := New()
-	player := playerbuilder.Build()
-	player.Draw("A")
-	player.Draw("A")
-	player.Draw("A")
-	player.Draw("A")
-	player.Draw("7")
-	if player.Compute() != 0 {
-		t.Fatalf("error: compute %v", player.Compute())
+	hand := [][]string{{"A", "A", "A", "A", "7"}, {"A", "J"}, {"K", "Q"}, {"10", "5"}}
+	result := []int{0, 0, 1, 6}
+	for i, cards := range hand {
+		playerbuilder := New()
+		player := playerbuilder.Build()
+		for _, c := range cards {
+			player.Draw(c)
+		}
+		if player.Compute() != result[i] {
+			t.Fatalf("error: compute %v", player.Compute())
+		}
 	}
-
 	t.Log("Pass Compute func")
 }
